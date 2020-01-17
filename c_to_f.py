ebc4db9494f6ca52b46e60900018c26beaaeb9f3
Tace <yalianwork@gmail.com>
2020-01-18 03:33:25 +0800

    add new file

---
+++ c_to_f.py
@@ -0,0 +1,4 @@
+ctemprature = input('please enter Celsius temprature: ')
+ctemprature =  float(ctemprature);
+ftemprature = ctemprature * 9 / 5 + 32
+print('The Fahrenheit temprature is: ', ftemprature)
